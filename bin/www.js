504789b69f42a76a76de182ccc121cf8ce61f037
ZierkCodes <zierk.gage@gmail.com>
2021-07-30 04:22:37 -0400

    Heroku Having Troubles with Socket.io

--- bin/www.js
+++ bin/www.js
@@ -19,7 +19,7 @@
   * Get port from environment and store in Express.
   */
  
- const port = normalizePort(process.env.PORT)
+ const port = normalizePort(process.env.PORT || '3000')
  app.set('port', port)
  
  /**
