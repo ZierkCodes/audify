fc3800128d629a73f1efcbdf06eed3d6a8fe286e
ZierkCodes <zierk.gage@gmail.com>
2021-07-30 02:41:49 -0400

    Implemented Channel Search Functionality

--- public/js/search.js
+++ public/js/search.js
@@ -2,12 +2,11 @@ let channels_array = document.querySelectorAll('.channel_item')
 let search_input = document.querySelector('#search_channels')
 
 search_input.addEventListener('keyup', (e) => {
-    console.log(search_input.value)
     channels_array.forEach((channel) => {
         if(channel.dataset.channel.includes(search_input.value)) {
-            channel.setAttribute('hidden', 'false')
+            channel.style.display = 'flex'
         } else {
-            channel.setAttribute('hidden', 'true')
+            channel.style.display = 'none'
         }
     })
 })
